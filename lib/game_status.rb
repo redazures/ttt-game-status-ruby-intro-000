3f5e8016738055cac0b3c302be3d201974b27cc7
james wu <jwu_nyc@yahoo.com>
2020-06-15 18:08:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -77,7 +77,7 @@ end
 
 puts define_win(WIN_COMBINATIONS[0],board10)
 puts iswin(board10)
-winlose=[]
+winlose=[define_win(top_row_win,board), define_win(mid_row_win,board), define_win(bot_row_win,board), define_win(left_col_win,board), define_win(mid_col_win,board), define_win(right_col_win,board), define_win(left_right_win,board), define_win(right_left_win,board)]
 puts winlose
 
 
