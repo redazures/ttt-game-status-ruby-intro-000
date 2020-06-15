29b250ef0421759b7390ef0c89c9bfe0eba857b3
james wu <jwu_nyc@yahoo.com>
2020-06-15 18:11:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -80,7 +80,9 @@ puts iswin(board10)
 win=[]
 winlose=[define_win(top_row_win,board), define_win(mid_row_win,board), define_win(bot_row_win,board), define_win(left_col_win,board), define_win(mid_col_win,board), define_win(right_col_win,board), define_win(left_right_win,board), define_win(right_left_win,board)]
 puts winlose
-
+winlose.each do |state|
+  
+end
 
 
 #won=false
